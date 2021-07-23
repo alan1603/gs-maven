aa21d85aa69dd53f70695a1f6dd4a882e622356c
alan1603 <47897719+alan1603@users.noreply.github.com>
2021-07-23 15:51:34 +0530

    Update HelloWorld.java

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -4,13 +4,24 @@ import org.joda.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		LocalTime currentTime = new LocalTime();
-		System.out.println("The current local time is: " + currentTime);
-		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHello());
-		TimeUnit.SECONDS.sleep(10000);
+	public static void main(String[] args)
+	 { 
+		try
+		{
+			LocalTime currentTime = new LocalTime();
+			System.out.println("The current local time is: " + currentTime);
+			Greeter greeter = new Greeter();
+			System.out.println(greeter.sayHello());
+			TimeUnit.SECONDS.sleep(10000);
+		}
+	  
+	  	catch(Exception ex)
+		{
+    		Thread.currentThread().interrupt();
+		}
 
 		
-	}
+	} 
+	
 }
+
